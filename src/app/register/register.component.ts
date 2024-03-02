425eb20668433c81392cc2205b2529f5635c82b8
Dang Duy <dangduy1402@gmail.com>
2024-03-02 16:48:57 +0700

    xong 50

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
-  imports: [FooterComponent, FormsModule],
+  imports: [FooterComponent, FormsModule, CommonModule],
 })
 export class RegisterComponent {
+  @ViewChild('registerForm') registerForm!: NgForm;
   // Khai báo các biến tương ứng với các trường dữ liệu trong form
   phone: string;
   password: string;
@@ -42,5 +45,37 @@ export class RegisterComponent {
       `isAccepted: ${this.isAccepted}` +
       `dateOfBirth: ${this.dateOfBirth}`;
     alert(message);
+  }
+  // how to check password match?
+  checkPasswordsMatch() {
+    if (this.password != this.retypePassword) {
+      this.registerForm.form.controls['retypePassword'].setErrors({
+        passwordMismatch: true,
+      });
+    } else {
+      this.registerForm.form.controls['retypePassword'].setErrors(null);
+    }
+  }
+  checkAge() {
+    if (this.dateOfBirth) {
+      const today = new Date();
+      const birthDate = new Date(this.dateOfBirth);
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const monthDiff = today.getMonth() - birthDate.getMonth();
+      if (
+        monthDiff < 0 ||
+        (monthDiff === 0 && today.getDate() < birthDate.getDate())
+      ) {
+        age--;
+      }
+
+      if (age < 18) {
+        this.registerForm.form.controls['dateOfBirth'].setErrors({
+          invalidAge: true,
+        });
+      } else {
+        this.registerForm.form.controls['dateOfBirth'].setErrors(null);
+      }
+    }
   }
 }
