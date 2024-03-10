e26807e08b4295e05eb8d2876cc70c6c539904df
Dang Duy <dangduy1402@gmail.com>
2024-03-10 23:40:25 +0700

    Filter by text and product category

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { ProductService } from '../../services/product.service';
+import { CategoryService } from '../../services/category.service';
 import { Product } from '../../models/product';
 import { Category } from '../../models/category';
 import { environment } from '../../environments/environment';
@@ -16,6 +17,7 @@ import { FormsModule } from '@angular/forms';
   imports: [FooterComponent, HeaderComponent, CommonModule, FormsModule],
 })
 export class HomeComponent implements OnInit {
+  [x: string]: any;
   products: Product[] = [];
   categories: Category[] = []; // Dữ liệu động từ categoryService
   selectedCategoryId: number = 0; // Giá trị category được chọn
@@ -27,43 +29,96 @@ export class HomeComponent implements OnInit {
   keyword: string = '';
   localStorage?: Storage;
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private categoryService: CategoryService
+  ) {}
 
   ngOnInit() {
-    this.getProducts(this.currentPage, this.itemsPerPage);
-    this.visiblePages = this.generateVisiblePageArray(this.currentPage, this.totalPages);
+    this.getProducts(
+      this.keyword,
+      this.selectedCategoryId,
+      this.currentPage,
+      this.itemsPerPage
+    );
+    this.getCategories(0, 100);
+    this.visiblePages = this.generateVisiblePageArray(
+      this.currentPage,
+      this.totalPages
+    );
   }
-  getProducts(page: number, limit: number) {
-    this.productService.getProducts(page, limit).subscribe({
-      next: (response: any) => {
+  getCategories(page: number, limit: number) {
+    this.categoryService.getCategories(page, limit).subscribe({
+      next: (categories: Category[]) => {
         debugger;
-        response.products.forEach((product: Product) => {
-          product.url = `${environment.apiBaseUrl}/products/images/${product.thumbnail}`;
-        });
-        this.products = response.products;
-        this.totalPages = response.totalPages;
-        this.visiblePages = this.generateVisiblePageArray(
-          this.currentPage,
-          this.totalPages
-        ); // Sửa đổi ở đây
+        this.categories = categories;
       },
       complete: () => {
         debugger;
       },
       error: (error: any) => {
-        debugger;
-        console.log('Error fetching products', error);
+        console.error('Error fetching categories:', error);
       },
     });
+  }
+  getProducts(
+    keyword: string,
+    selectedCategoryId: number,
+    page: number,
+    limit: number
+  ) {
+    this.productService
+      .getProducts(keyword, selectedCategoryId, page, limit)
+      .subscribe({
+        next: (response: any) => {
+          debugger;
+          response.products.forEach((product: Product) => {
+            product.url = `${environment.apiBaseUrl}/products/images/${product.thumbnail}`;
+          });
+          this.products = response.products;
+          this.totalPages = response.totalPages;
+          this.visiblePages = this.generateVisiblePageArray(
+            this.currentPage,
+            this.totalPages
+          ); // Sửa đổi ở đây
+        },
+        complete: () => {
+          debugger;
+        },
+        error: (error: any) => {
+          debugger;
+          console.log('Error fetching products', error);
+        },
+      });
+  }
+
+  searchProducts() {
+    this.currentPage = 0;
+    this.itemsPerPage = 12;
+    debugger;
+    this.getProducts(
+      this.keyword,
+      this.selectedCategoryId,
+      this.currentPage,
+      this.itemsPerPage
+    );
   }
 
   onPageChange(page: number) {
     debugger;
     this.currentPage = page < 0 ? 0 : page;
-    this.localStorage?.setItem('currentProductPage', String(this.currentPage));
-    this.getProducts(this.currentPage, this.itemsPerPage);
+    // this.localStorage?.setItem('currentProductPage', String(this.currentPage));
+    this.getProducts(
+      this.keyword,
+      this.selectedCategoryId,
+      this.currentPage,
+      this.itemsPerPage
+    );
 
-    this.visiblePages = this.generateVisiblePageArray(this.currentPage, this.totalPages);
+    this.visiblePages = this.generateVisiblePageArray(
+      this.currentPage,
+      this.totalPages
+    );
   }
 
   generateVisiblePageArray(currentPage: number, totalPages: number): number[] {
